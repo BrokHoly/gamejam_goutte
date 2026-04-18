012d10da627e11e76eebb0e3c7193b8c42f8f3e5
Fisselo <77895882+Fiss53@users.noreply.github.com>
2026-04-18 15:06:29 +0200

    ui  + theme

---
+++ menu/menu_manager.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+
+
+func _on_demarrer_pressed() -> void:
+	#TODO go to main scene
+	pass
+
+
+func _on_quitter_pressed() -> void:
+	get_tree().quit()
---
+++ menu/skin_selector.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var base_skin: Sprite2D = $Sprite2D;
+
+
+
+func _ready() -> void:
+	pass
+	
