ce09176114adc4880c65786795e1e0faeca95a0e
BrokHoly <62349056+BrokHoly@users.noreply.github.com>
2026-04-19 12:57:05 +0200

    drop particules adaptative + shapes + sprite

--- player/drop.gd
+++ player/drop.gd
@@ -14,16 +14,21 @@ var LIQUID_SHAPE: CollisionShape2D
 var ICE_SHAPE: CollisionShape2D
 var SNOW_SHAPE: CollisionShape2D
 
-const LIQUID_PARTICLES : Color = Color("4995f3")
-const ICE_PARTICLES : Color = Color("8d9bc7cd")
-const SNOW_PARTICLES : Color = Color("dfecfeff")
+const LIQUID_COLOR : Color = Color("4995f3")
+const ICE_COLOR : Color = Color("8d9bc7cd")
+const SNOW_COLOR : Color = Color("dfecfeff")
 
 var DROP_PARTICULES: CPUParticles2D
+var SPLASH_PARTICULES: CPUParticles2D
 
 var state = STATES.LIQUID
 
 var sprite : Sprite2D
 
+const TRANSITION_TIME: float = 0.5
+var transition_amout: float = 0.0
+var baseColor: Color
+var targetColor: Color
 
 func _ready() -> void:
 	LIQUID_SKIN = GameManager.playerSkin
@@ -43,7 +48,11 @@ func _ready() -> void:
 	SNOW_SPRITE.hide()
 	sprite = LIQUID_SPRITE
 	
+	baseColor = LIQUID_COLOR
+	targetColor = LIQUID_COLOR
+	
 	DROP_PARTICULES = $DropParticules
+	SPLASH_PARTICULES = $SplashParticules
 	
 	GameManager.health_changed.connect(take_damage)
 
@@ -54,7 +63,9 @@ func _physics_process(delta: float) -> void:
 	
 func _process(delta: float) -> void:
 	SNOW_SPRITE.rotation += 0.05
-
+	if transition_amout <= TRANSITION_TIME:
+		transition_amout += delta
+		color_transition(baseColor, targetColor, transition_amout / TRANSITION_TIME)
 
 
 func take_damage(amount : int):
@@ -71,8 +82,6 @@ func take_damage(amount : int):
 
 		tweenScale.tween_property(sprite, "scale", initial_scale, 0.25)
 		tweenRed.tween_property(sprite,"modulate",initial_color,0.25)
-	
-
 
 
 
@@ -107,6 +116,9 @@ func _toggle_ice():
 	if state == STATES.ICE:
 		activate_liquid()
 	else:
+		baseColor = targetColor
+		targetColor = ICE_COLOR
+		transition_amout = 0.0
 		state = STATES.ICE
 		sprite = ICE_SPRITE
 		ICE_SPRITE.show()
@@ -122,6 +134,9 @@ func _toggle_snow():
 	if state == STATES.SNOW:
 		activate_liquid()
 	else:
+		baseColor = targetColor
+		targetColor = SNOW_COLOR
+		transition_amout = 0.0
 		state = STATES.SNOW
 		SNOW_SPRITE.show()
 		ICE_SPRITE.hide()
@@ -132,6 +147,9 @@ func _toggle_snow():
 		ICE_SPRITE.visible = false
 
 func activate_liquid():
+	baseColor = targetColor
+	targetColor = LIQUID_COLOR
+	transition_amout = 0.0
 	state = STATES.LIQUID
 	sprite = LIQUID_SPRITE
 	LIQUID_SPRITE.show()
@@ -152,5 +170,7 @@ func is_ice() -> bool :
 	return state == STATES.ICE
 
 
-func color_transition(color: Color):
-	DROP_PARTICULES.color.lerp(color,1.0)
+func color_transition(baseColor: Color, targetColor:Color, amout: float):
+	print(baseColor)
+	DROP_PARTICULES.color = baseColor.lerp(targetColor,amout)
+	SPLASH_PARTICULES.color = baseColor.lerp(targetColor,amout)
