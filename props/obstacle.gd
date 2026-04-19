16cc3219f7224f66d8823cd8fe1327f23928ab9b
Fisselo <77895882+Fiss53@users.noreply.github.com>
2026-04-19 15:47:24 +0200

    fix les pb de noms de grouoe

--- props/obstacle.gd
+++ props/obstacle.gd
@@ -2,10 +2,16 @@ extends Node2D
 
 
 var speed = 0.0
-
+var audio_web_success : AudioStreamPlayer2D
+var audio_leaf_success : AudioStreamPlayer2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	audio_web_success = AudioStreamPlayer2D.new()
+	audio_leaf_success = AudioStreamPlayer2D.new()
+	audio_web_success.stream = load("res://Assets/Audio/web_succcess.mp3")
+	audio_leaf_success.stream = load("res://Assets/Audio/leaf_success.mp3")
+	add_child(audio_web_success)
+	add_child(audio_leaf_success)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,10 +27,12 @@ func _on_hit_box_body_entered(body: Node2D) -> void:
 		if body.is_in_group("Player1"):
 			if body.is_ice() and is_in_group("breakable"):
 				GameManager.score += 100
+				audio_leaf_success.play()
 				#TODO make an animation or something
 				pass
-			if body.is_liquid() and is_in_group("travesable"):
+			elif body.is_liquid() and is_in_group("traversable"):
 				GameManager.score += 100
+				audio_web_success.play()
 				#TODO make an animation or something
 				pass 
 			else :
@@ -35,7 +43,7 @@ func _on_hit_box_body_entered(body: Node2D) -> void:
 				GameManager.scoreP2 += 100
 				#TODO make an animation or something
 				pass
-			if body.is_liquid() and is_in_group("travesable"):
+			elif body.is_liquid() and is_in_group("traversable"):
 				GameManager.scoreP2 += 100
 				#TODO make an animation or something
 				pass 
@@ -48,7 +56,7 @@ func _on_hit_box_body_entered(body: Node2D) -> void:
 
 
 func _on_spawn_box_area_entered(area: Area2D) -> void:
-	if area.is_in_group("invisible") and is_in_group("invincible"):
+	if area.is_in_group("invincible") and is_in_group("invincible"):
 		# In taht case, we should stop the belower until it's out of range. 
 		pass
 		
